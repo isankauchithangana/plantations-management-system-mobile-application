8b4aab68ebc5fd19f6c167bca26ef7e01cd0a4b8
Isanka Uchithangana Hapuarachchi <106378990+isankauchithangana@users.noreply.github.com>
2024-03-17 09:05:40 +0530

    Update constant.dart

--- lib/bdHelper/constant.dart
+++ lib/bdHelper/constant.dart
@@ -2,14 +2,14 @@
 //password = "ccJF77cF4niPFapi"
 
 const MONGO_CONN_URL =
-    "mongodb+srv://chanaka:ccJF77cF4niPFapi@cluster0.id2qofj.mongodb.net/smartfarm?retryWrites=true&w=majority";
+    "mongodb+srv://<username>:<password>@<hostname>/smartfarm?retryWrites=true&w=majority";
 
 const USER_COLLECTION = "test";
 
 const TEST_MONGO_CONN_URL =
-    "mongodb+srv://chanaka:ccJF77cF4niPFapi@cluster0.id2qofj.mongodb.net/test?retryWrites=true&w=majority";
+    "mongodb+srv://<username>:<password>@<hostname>/admin?retryWrites=true&w=majority";
 
 const TEST_USER_COLLECTION = "cultivations";
 
-const String OPENWEATHER_API_KEY = "45b8ae655a27b6d52e54ac8f725b7230";
-const String GOOGLE_MAPS_API_KEY = 'AIzaSyBq1-RhZ6MKh6Y-TDOvOimh6BdHDzSHosw';
+const String OPENWEATHER_API_KEY = "<API Key>";
+const String GOOGLE_MAPS_API_KEY = '<API Key>';
